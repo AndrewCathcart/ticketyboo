2acced362eab2c6730a9e98c97c65270a02ce455
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-09-13 17:50:10 +0100

    using express-async-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -17,7 +18,7 @@ app.use(signOutRouter);
 app.use(signUpRouter);
 
 app.all('*', async (req, res, next) => {
-  next(new NotFoundError());
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
