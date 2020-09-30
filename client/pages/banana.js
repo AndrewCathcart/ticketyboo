cfe8e2c426522a612d323ab763130bb89414f614
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-10-01 00:17:43 +0100

    Nextjs running in kubernetes cluster

---
+++ client/pages/banana.js
@@ -0,0 +1,5 @@
+const Banana = () => {
+  return <h1>Banana</h1>;
+};
+
+export default Banana;
---
+++ client/pages/index.js
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+  return <h1>Landing Page</h1>;
+};
+
+export default LandingPage;
