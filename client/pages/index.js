ade06689e5ba09328f2d2b2303867cceece67128
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-07 18:08:11 +0000

    refactored client/server req slightly

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,21 +1,12 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  const headers = req.headers;
-  const serverUrl =
-    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
-  const browserUrl = '/api/users/currentuser';
-
-  const { data } =
-    typeof window === 'undefined'
-      ? await axios.get(serverUrl, { headers })
-      : await axios.get(browserUrl);
-
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
   return data;
 };
 
