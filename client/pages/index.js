3eb588e1a37c860f63c4c970f9c94afec8f9ea4d
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-07 18:09:00 +0000

    readability

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,7 +6,9 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get('/api/users/currentuser');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
   return data;
 };
 
