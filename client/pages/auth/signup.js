48474ae1c03dded0609c683e74a18e8552e37488
Andrew Cathcart <newcastleandrewc@gmail.com>
2020-11-07 17:57:52 +0000

    server / client requests with ingress-nginx

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -22,28 +22,30 @@ const SignUp = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
-      <div className='form-group'>
-        <label>Email Address</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className='form-control'
-        />
-      </div>
-      <div className='form-group'>
-        <label>Password</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type='password'
-          className='form-control'
-        />
-      </div>
-      {errors}
-      <button className='btn btn-primary'>Sign Up</button>
-    </form>
+    <div className='container'>
+      <form onSubmit={onSubmit} className='w-50 mx-auto mt-2'>
+        <h1>Sign Up</h1>
+        <div className='form-group'>
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='form-control'
+          />
+        </div>
+        <div className='form-group'>
+          <label>Password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            className='form-control'
+          />
+        </div>
+        {errors}
+        <button className='btn btn-primary'>Sign Up</button>
+      </form>
+    </div>
   );
 };
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,22 @@
-const LandingPage = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  const headers = req.headers;
+  const serverUrl =
+    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
+  const browserUrl = '/api/users/currentuser';
+
+  const { data } =
+    typeof window === 'undefined'
+      ? await axios.get(serverUrl, { headers })
+      : await axios.get(browserUrl);
+
+  return data;
 };
 
 export default LandingPage;
